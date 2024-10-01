260b8e7136852cef2e35a2a27b9bb395452f53e4
MikaSRahwono <70706750+MikaSRahwono@users.noreply.github.com>
2024-10-01 14:23:32 +0700

    Initial Commit

---
+++ MantaDiveiOSAppTests/MantaDiveiOSAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MantaDiveiOSAppTests.swift
+//  MantaDiveiOSAppTests
+//
+//  Created by Mika S Rahwono on 01/10/24.
+//
+
+import Testing
+@testable import MantaDiveiOSApp
+
+struct MantaDiveiOSAppTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
